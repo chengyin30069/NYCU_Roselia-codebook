8aef7fc53aa18adac72d47aa8063abc17e55cdf9
Linus <chengyin30069@gmail.com>
2025-05-04 01:16:01 +0800

    [Update]
    Updated Dinic, MCMF and KM

---
+++ code/flow/KM.cpp
@@ -0,0 +1,66 @@
+struct KM
+{
+	int n,mx[1005],my[1005],pa[1005];
+	int g[1005][1005],lx[1005],ly[1005],sy[1005];
+	bool vx[1005],vy[1005];
+	void init(int _n)
+	{
+		n=_n;
+		FOR(i,1,n+1)fill(g[i],g[i]+1+n,0);
+	}
+	void add(int a,int b,int c){g[a][b]=c;}
+	void augment(int y)
+	{
+		for(int x,z;y;y=z)x=pa[y],z=mx[x],my[y]=x,mx[x]=y;
+	}
+	void bfs(int st)
+	{
+		FOR(i,1,n+1)sy[i]=INF,vx[i]=vy[i]=0;
+		queue<int>q;q.push(st);
+		for(;;)
+		{
+			while(!q.empty())
+			{
+				int x=q.front();q.pop();
+				vx[x]=1;
+				FOR(y,1,n+1)if(!vy[y])
+				{
+					int t=lx[x]+ly[y]-g[x][y];
+					if(t==0)
+					{
+						pa[y]=x;
+						if(!my[y]){augment(y);return;}
+						vy[y]=1, q.push(my[y]);
+					}
+					else if(sy[y]>t)pa[y]=x,sy[y]=t;
+				}
+			}
+			int cut=INF;
+			FOR(y,1,n+1)if(!vy[y]&&cut>sy[y])cut=sy[y];
+			FOR(j,1,n+1)
+			{
+				if(vx[j])lx[j]-=cut;
+				if(vy[j])ly[j]+=cut;
+				else sy[j]-=cut;
+			}
+			FOR(y,1,n+1)
+			{
+				if(!vy[y]&&sy[y]==0)
+				{
+					if(!my[y]){augment(y);return;}
+					vy[y]=1;q.push(my[y]);
+				}
+			}
+		}
+	}
+	int solve()
+	{
+		fill(mx,mx+n+1,0);fill(my,my+n+1,0);
+		fill(ly,ly+n+1,0);fill(lx,lx+n+1,0);
+		FOR(x,1,n+1)FOR(y,1,n+1)lx[x]=max(lx[x],g[x][y]);
+		FOR(x,1,n+1)bfs(x);
+		int ans=0;
+		FOR(y,1,n+1)ans+=g[my[y]][y];
+		return ans;
+	}
+};
---
+++ code/flow/MCMF2.cpp
@@ -0,0 +1,63 @@
+struct MCMF
+{
+	int n,s,t,par[N+5],p_i[N+5],dis[N+5],vis[N+5];
+	struct edge{int to,cap,rev,cost;};
+	vector<edge>path[N];
+	void init(int _n,int _s,int _t)
+	{
+		n=_n,s=_s,t=_t;
+		FOR(i,0,2*n+5)par[i]=p_i[i]=vis[i]=0;
+	}
+	void add(int a,int b,int c,int d)
+	{
+		path[a].pb({b,c,sz(path[b]),d});
+		path[b].pb({a,0,sz(path[a])-1,-d});
+	}
+	void spfa()
+	{
+		FOR(i,0,n*2+5)dis[i]=INF,vis[i]=0;
+		dis[s]=0;
+		queue<int>q;q.push(s);
+		while(!q.empty())
+		{
+			int now=q.front();
+			q.pop();
+			vis[now]=0;
+			for(int i=0;i<sz(path[now]);i++)
+			{
+				edge e=path[now][i];
+				if(e.cap>0&&dis[e.to]>dis[now]+e.cost)
+				{
+					dis[e.to]=dis[now]+e.cost;
+					par[e.to]=now;
+					p_i[e.to]=i;
+					if(vis[e.to]==0)
+					{
+						vis[e.to]=1;
+						q.push(e.to);
+					}
+				}
+			}
+		}
+	}
+	pii flow()
+	{
+		int flow=0,cost=0;
+		while(true)
+		{
+			spfa();
+			if(dis[t]==INF)break;
+			int mn=INF;
+			for(int i=t;i!=s;i=par[i])
+				mn=min(mn,path[par[i]][p_i[i]].cap);
+			flow+=mn;cost+=dis[t]*mn;
+			for(int i=t;i!=s;i=par[i])
+			{
+				edge &now=path[par[i]][p_i[i]];
+				now.cap-=mn;
+				path[i][now.rev].cap+=mn;
+			}
+		}
+		return mp(flow,cost);
+	}
+};
