25694df738e801e0d2fb281df1205325bcd539d0
howardlee <howard20051109@gmail.com>
2025-05-23 11:06:26 +0800

    add FFT kosaraju

---
+++ code/graph/SCC-kosaraju.cpp
@@ -0,0 +1,44 @@
+const int N = 1e5 + 10;
+vector<int> ed[N], ed_b[N]; // 反邊
+vector<int> SCC(N); // 最後SCC的分組
+bitset<N> vis;
+int SCC_cnt;
+int n, m;
+vector<int> pre; // 後序遍歷
+
+void dfs(int x)
+{
+    vis[x] = 1;
+    for(int i : ed[x]) {
+        if(vis[i]) continue;
+        dfs(i);
+    }
+    pre.push_back(x);
+}
+
+void dfs2(int x)
+{
+    vis[x] = 1;
+    SCC[x] = SCC_cnt;
+    for(int i : ed_b[x]) {
+        if(vis[i]) continue;
+        dfs2(i);
+    }
+}
+
+void kosaraju()
+{
+    for(int i = 1; i <= n; i++) {
+        if(!vis[i]) {
+            dfs(i);
+        }
+    }
+    SCC_cnt = 0;
+    vis = 0;
+    for(int i = n - 1; i >= 0; i--) {
+        if(!vis[pre[i]]) {
+            SCC_cnt++;
+            dfs2(pre[i]);
+        }
+    }
+}
--- code/number-theory/FFT.cpp
+++ code/number-theory/FFT.cpp
@@ -50,3 +50,64 @@ struct FastFourierTransform{
 	void fft(vector<cp> &a){ transform(a,omega); }
 	void ifft(vector<cp> &a){ transform(a,iomega); for(int i = 0;i < n;i++) a[i] /= n;}
 } FFT;
+
+
+const int MAXN = 262144;
+// (must be 2^k)
+// 262144, 524288, 1048576, 2097152, 4194304
+// before any usage, run pre_fft() first
+typedef long double ld;
+typedef complex<ld> cplx; //real() ,imag()
+const ld PI = acosl(-1);
+const cplx I(0, 1);
+cplx omega[MAXN+1];
+void pre_fft(){
+    for(int i=0; i<=MAXN; i++) {
+        omega[i] = exp(i * 2 * PI / MAXN * I);
+    }
+}
+// n must be 2^k
+void fft(int n, cplx a[], bool inv=false){
+    int basic = MAXN / n; 
+    int theta = basic;
+    for (int m = n; m >= 2; m >>= 1) {
+        int mh = m >> 1;
+        for (int i = 0; i < mh; i++) {
+            cplx w = omega[inv ? MAXN - (i * theta % MAXN) : i * theta % MAXN];
+            for (int j = i; j < n; j += m) {
+                int k = j + mh;
+                cplx x = a[j] - a[k];
+                a[j] += a[k];
+                a[k] = w * x;
+            } 
+        }
+        theta = (theta * 2) % MAXN;
+    }
+    int i = 0;
+    for (int j = 1; j < n - 1; j++) {
+        for (int k = n >> 1; k > (i ^= k); k >>= 1);
+        if (j < i) swap(a[i], a[j]);
+    }
+    if(inv) {
+        for (i = 0; i < n; i++) a[i] /= n;
+    }
+}
+cplx arr[MAXN + 1];
+inline void mul(int _n,long long a[],int _m,long long b[],long long ans[]){
+    int n=1, sum = _n + _m - 1;
+    while(n < sum) n <<= 1;
+    for(int i = 0; i < n; i++) {
+        double x= (i < _n ? a[i] : 0), y=(i < _m ? b[i] : 0);
+        arr[i] = complex<double>(x + y, x - y);
+    }
+    fft(n, arr);
+    for(int i = 0; i < n; i++) arr[i]=arr[i]*arr[i];
+    fft(n,arr,true);
+    for(int i=0;i<sum;i++) ans[i]=(long long int)(arr[i].real() / 4 + 0.5);
+}
+
+long long a[MAXN];
+long long b[MAXN];
+long long ans[MAXN];
+int a_length;
+int b_length;
