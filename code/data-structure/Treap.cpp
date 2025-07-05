89728c6adfb7d83537b48c4ef944c58cf957f92b
Linus <chengyin30069@gmail.com>
2025-07-05 15:39:39 +0800

    [Update]
    Formatted data structure code

--- code/data-structure/Treap.cpp
+++ code/data-structure/Treap.cpp
@@ -1,81 +1,64 @@
 mt19937 rng(random_device{}());
-struct Treap
-{
-	Treap *l,*r;
-	int val,num,pri;
-	Treap(int k)
-	{
-		l=r=NULL;
-		val=k;
-		num=1;
-		pri=rng();
-	}
+struct Treap {
+    Treap *l, *r;
+    int val, num, pri;
+    Treap(int k) {
+        l = r = NULL;
+        val = k;
+        num = 1;
+        pri = rng();
+    }
 };
-int siz(Treap *now){return now?now->num:0;}
-void pull(Treap *&now)
-{
-	now->num=siz(now->l)+siz(now->r)+1;
+int siz(Treap *now) { return now ? now->num : 0; }
+void pull(Treap *&now) {
+    now->num = siz(now->l) + siz(now->r) + 1;
 }
-Treap* merge(Treap *a,Treap *b)
-{
-	if(!a||!b)return a?a:b;
-	else if(a->pri>b->pri)
-	{
-		a->r=merge(a->r,b);
-		pull(a);
-		return a;
-	}
-	else 
-	{
-		b->l=merge(a,b->l);
-		pull(b);
-		return b;
-	}
+Treap *merge(Treap *a, Treap *b) {
+    if (!a || !b)
+        return a ? a : b;
+    else if (a->pri > b->pri) {
+        a->r = merge(a->r, b);
+        pull(a);
+        return a;
+    } else {
+        b->l = merge(a, b->l);
+        pull(b);
+        return b;
+    }
 }
-void split_size(Treap *rt,Treap *&a,Treap *&b,int val)
-{
-	if(!rt)
-	{
-		a=b=NULL;
-		return;
-	}
-	if(siz(rt->l)+1>val)
-	{
-		b=rt;
-		split_size(rt->l,a,b->l,val);
-		pull(b);
-	}
-	else 
-	{
-		a=rt;
-		split_size(rt->r,a->r,b,val-siz(a->l)-1);
-		pull(a);
-	}
+void split_size(Treap *rt, Treap *&a, Treap *&b, int val) {
+    if (!rt) {
+        a = b = NULL;
+        return;
+    }
+    if (siz(rt->l) + 1 > val) {
+        b = rt;
+        split_size(rt->l, a, b->l, val);
+        pull(b);
+    } else {
+        a = rt;
+        split_size(rt->r, a->r, b, val - siz(a->l) - 1);
+        pull(a);
+    }
 }
-void split_val(Treap *rt,Treap *&a,Treap *&b,int val)
-{
-	if(!rt)
-	{
-		a=b=NULL;
-		return;
-	}
-	if(rt->val<=val)
-	{
-		a=rt;
-		split_val(rt->r,a->r,b,val);
-		pull(a);
-	}
-	else
-	{
-		b=rt;
-		split_val(rt->l,a,b->l,val);
-		pull(b);
-	}
+void split_val(Treap *rt, Treap *&a, Treap *&b, int val) {
+    if (!rt) {
+        a = b = NULL;
+        return;
+    }
+    if (rt->val <= val) {
+        a = rt;
+        split_val(rt->r, a->r, b, val);
+        pull(a);
+    } else {
+        b = rt;
+        split_val(rt->l, a, b->l, val);
+        pull(b);
+    }
 }
-void treap_dfs(Treap *now)
-{
-	if(!now)return;
-	treap_dfs(now->l);
-	cout<<now->val<<" ";
-	treap_dfs(now->r);
+void treap_dfs(Treap *now) {
+    if (!now) return;
+    treap_dfs(now->l);
+    cout << now->val << " ";
+    treap_dfs(now->r);
 }
--- code/data-structure/time-segtree.cpp
+++ code/data-structure/time-segtree.cpp
@@ -14,14 +14,18 @@ inline bool merge(int x, int y) {
     return true;
 };
 inline void undo() {
-    auto [a, b, s] = his.back(); his.pop_back();
+    auto [a, b, s] = his.back();
+    his.pop_back();
     dsu[a] = a, sz[b] = s;
 }
 #define m ((l + r) >> 1)
 void insert(int ql, int qr, P<int> x, int i = 1, int l = 0, int r = q) {
     // debug(ql, qr, x); return;
     if (qr <= l || r <= ql) return;
-    if (ql <= l && r <= qr) {arr[i].push_back(x); return;}
+    if (ql <= l && r <= qr) {
+        arr[i].push_back(x);
+        return;
+    }
     if (qr <= m)
         insert(ql, qr, x, i << 1, l, m);
     else if (m <= ql)
@@ -37,7 +41,8 @@ void traversal(V<int>& ans, int i = 1, int l = 0, int r = q) {
     for (auto [a, b] : arr[i])
         if (merge(a, b))
             opcnt++, cnt--;
-    if (r - l == 1) ans[l] = cnt;
+    if (r - l == 1)
+        ans[l] = cnt;
     else {
         traversal(ans, i << 1, l, m);
         traversal(ans, i << 1 | 1, m, r);
@@ -48,41 +53,42 @@ void traversal(V<int>& ans, int i = 1, int l = 0, int r = q) {
 }
 #undef m
 inline void solve() {
-    int n, m; cin>>n>>m>>q,q++;
+    int n, m;
+    cin >> n >> m >> q, q++;
     dsu.resize(cnt = n), sz.assign(n, 1);
     iota(dsu.begin(), dsu.end(), 0);
     // a, b, time, operation
     unordered_map<ll, V<int>> s;
     for (int i = 0; i < m; i++) {
-        int a, b; cin>>a>>b;
+        int a, b;
+        cin >> a >> b;
         if (a > b) swap(a, b);
         s[((ll)a << 32) | b].emplace_back(0);
     }
     for (int i = 1; i < q; i++) {
-        int op,a, b;
-        cin>>op>>a>>b;
+        int op, a, b;
+        cin >> op >> a >> b;
         if (a > b) swap(a, b);
         switch (op) {
-        case 1:
-            s[((ll)a << 32) | b].push_back(i);
-            break;
-        case 2:
-            auto tmp = s[((ll)a << 32) | b].back();
-            s[((ll)a << 32) | b].pop_back();
-            insert(tmp, i, P<int> {a, b});
+            case 1:
+                s[((ll)a << 32) | b].push_back(i);
+                break;
+            case 2:
+                auto tmp = s[((ll)a << 32) | b].back();
+                s[((ll)a << 32) | b].pop_back();
+                insert(tmp, i, P<int>{a, b});
         }
     }
     for (auto [p, v] : s) {
         int a = p >> 32, b = p & -1;
         while (v.size()) {
-            insert(v.back(), q, P<int> {a, b});
+            insert(v.back(), q, P<int>{a, b});
             v.pop_back();
         }
     }
     V<int> ans(q);
     traversal(ans);
     for (auto i : ans)
-        cout<<i<<' ';
-    cout<<endl;
+        cout << i << ' ';
+    cout << endl;
 }
-
