965207dc4f6b91024661ae6b9fd4bdb7304a6444
Linus <chengyin30069@gmail.com>
2025-07-05 14:49:06 +0800

    [Update]
    Add gp_hash_table and Fix Makefile

--- code/basic/PBDS.cpp
+++ code/basic/PBDS.cpp
@@ -11,6 +11,12 @@ tree<int, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update> tr;
 tr.order_of_key(element);
 tr.find_by_order(rank);
 
+// hash table
+gp_hash_table<int, int> ht;
+ht.find(element);
+ht.insert({key, value});
+ht.erase(element);
+
 // priority queue
 __gnu_pbds::priority_queue<int, less<int> > big_q;  // Big First
 __gnu_pbds::priority_queue<int, greater<int> > small_q;  // Small First
