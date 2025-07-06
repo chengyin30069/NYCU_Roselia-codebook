56edb2bc0d9893a208903cef2626a3e34b62c0c6
Linus <chengyin30069@gmail.com>
2025-07-06 17:34:36 +0800

    [Update]

--- code/number-theory/FFT.cpp
+++ code/number-theory/FFT.cpp
@@ -3,72 +3,74 @@ typedef complex<double> cp;
 const double pi = acos(-1);
 const int NN = 131072;
 
-struct FastFourierTransform{
-	/*
-		Iterative Fast Fourier Transform
-		How this works? Look at this
-		0th recursion 0(000)   1(001)   2(010)   3(011)   4(100)   5(101)   6(110)   7(111)
-		1th recursion 0(000)   2(010)   4(100)   6(110) | 1(011)   3(011)   5(101)   7(111)
-		2th recursion 0(000)   4(100) | 2(010)   6(110) | 1(011)   5(101) | 3(011)   7(111)
-		3th recursion 0(000) | 4(100) | 2(010) | 6(110) | 1(011) | 5(101) | 3(011) | 7(111)
-		All the bits are reversed => We can save the reverse of the numbers in an array!
-	*/
-	int n, rev[NN];
-	cp omega[NN], iomega[NN];
-	void init(int n_){
-		n = n_;
-		for(int i = 0;i < n_;i++){
-			//Calculate the nth roots of unity
-			omega[i] = cp(cos(2*pi*i/n_),sin(2*pi*i/n_));
-			iomega[i] = conj(omega[i]);
-		}
-		int k = __lg(n_);
-		for(int i = 0;i < n_;i++){
-			int t = 0;
-			for(int j = 0;j < k;j++){
-				if(i & (1<<j)) t |= (1<<(k-j-1));
-			}
-			rev[i] = t;
-		}
-	}
+struct FastFourierTransform {
+    /*
+            Iterative Fast Fourier Transform
+            How this works? Look at this
+            0th recursion 0(000)   1(001)   2(010)   3(011)   4(100)   5(101)   6(110)   7(111)
+            1th recursion 0(000)   2(010)   4(100)   6(110) | 1(011)   3(011)   5(101)   7(111)
+            2th recursion 0(000)   4(100) | 2(010)   6(110) | 1(011)   5(101) | 3(011)   7(111)
+            3th recursion 0(000) | 4(100) | 2(010) | 6(110) | 1(011) | 5(101) | 3(011) | 7(111)
+            All the bits are reversed => We can save the reverse of the numbers in an array!
+    */
+    int n, rev[NN];
+    cp omega[NN], iomega[NN];
+    void init(int n_) {
+        n = n_;
+        for (int i = 0; i < n_; i++) {
+            // Calculate the nth roots of unity
+            omega[i] = cp(cos(2 * pi * i / n_), sin(2 * pi * i / n_));
+            iomega[i] = conj(omega[i]);
+        }
+        int k = __lg(n_);
+        for (int i = 0; i < n_; i++) {
+            int t = 0;
+            for (int j = 0; j < k; j++) {
+                if (i & (1 << j)) t |= (1 << (k - j - 1));
+            }
+            rev[i] = t;
+        }
+    }
 
-	void transform(vector<cp> &a, cp* xomega){
-		for(int i = 0;i < n;i++)
-			if(i < rev[i]) swap(a[i],a[rev[i]]);
-		for(int len = 2; len <= n; len <<= 1){
-			int mid = len >> 1;
-			int r = n/len;
-			for(int j = 0;j < n;j += len)
-				for(int i = 0;i < mid;i++){
-					cp tmp = xomega[r*i] * a[j+mid+i];
-					a[j+mid+i] = a[j+i] - tmp;
-					a[j+i] = a[j+i] + tmp;
-				}
-		}
-	}
+    void transform(vector<cp> &a, cp *xomega) {
+        for (int i = 0; i < n; i++)
+            if (i < rev[i]) swap(a[i], a[rev[i]]);
+        for (int len = 2; len <= n; len <<= 1) {
+            int mid = len >> 1;
+            int r = n / len;
+            for (int j = 0; j < n; j += len)
+                for (int i = 0; i < mid; i++) {
+                    cp tmp = xomega[r * i] * a[j + mid + i];
+                    a[j + mid + i] = a[j + i] - tmp;
+                    a[j + i] = a[j + i] + tmp;
+                }
+        }
+    }
 
-	void fft(vector<cp> &a){ transform(a,omega); }
-	void ifft(vector<cp> &a){ transform(a,iomega); for(int i = 0;i < n;i++) a[i] /= n;}
+    void fft(vector<cp> &a) { transform(a, omega); }
+    void ifft(vector<cp> &a) {
+        transform(a, iomega);
+        for (int i = 0; i < n; i++) a[i] /= n;
+    }
 } FFT;
-
 
 const int MAXN = 262144;
 // (must be 2^k)
 // 262144, 524288, 1048576, 2097152, 4194304
 // before any usage, run pre_fft() first
 typedef long double ld;
-typedef complex<ld> cplx; //real() ,imag()
+typedef complex<ld> cplx;  // real() ,imag()
 const ld PI = acosl(-1);
 const cplx I(0, 1);
-cplx omega[MAXN+1];
-void pre_fft(){
-    for(int i=0; i<=MAXN; i++) {
+cplx omega[MAXN + 1];
+void pre_fft() {
+    for (int i = 0; i <= MAXN; i++) {
         omega[i] = exp(i * 2 * PI / MAXN * I);
     }
 }
 // n must be 2^k
-void fft(int n, cplx a[], bool inv=false){
-    int basic = MAXN / n; 
+void fft(int n, cplx a[], bool inv = false) {
+    int basic = MAXN / n;
     int theta = basic;
     for (int m = n; m >= 2; m >>= 1) {
         int mh = m >> 1;
@@ -79,7 +81,7 @@ void fft(int n, cplx a[], bool inv=false){
                 cplx x = a[j] - a[k];
                 a[j] += a[k];
                 a[k] = w * x;
-            } 
+            }
         }
         theta = (theta * 2) % MAXN;
     }
@@ -88,22 +90,22 @@ void fft(int n, cplx a[], bool inv=false){
         for (int k = n >> 1; k > (i ^= k); k >>= 1);
         if (j < i) swap(a[i], a[j]);
     }
-    if(inv) {
+    if (inv) {
         for (i = 0; i < n; i++) a[i] /= n;
     }
 }
 cplx arr[MAXN + 1];
-inline void mul(int _n,long long a[],int _m,long long b[],long long ans[]){
-    int n=1, sum = _n + _m - 1;
-    while(n < sum) n <<= 1;
-    for(int i = 0; i < n; i++) {
-        double x= (i < _n ? a[i] : 0), y=(i < _m ? b[i] : 0);
+inline void mul(int _n, long long a[], int _m, long long b[], long long ans[]) {
+    int n = 1, sum = _n + _m - 1;
+    while (n < sum) n <<= 1;
+    for (int i = 0; i < n; i++) {
+        double x = (i < _n ? a[i] : 0), y = (i < _m ? b[i] : 0);
         arr[i] = complex<double>(x + y, x - y);
     }
     fft(n, arr);
-    for(int i = 0; i < n; i++) arr[i]=arr[i]*arr[i];
-    fft(n,arr,true);
-    for(int i=0;i<sum;i++) ans[i]=(long long int)(arr[i].real() / 4 + 0.5);
+    for (int i = 0; i < n; i++) arr[i] = arr[i] * arr[i];
+    fft(n, arr, true);
+    for (int i = 0; i < sum; i++) ans[i] = (long long int)(arr[i].real() / 4 + 0.5);
 }
 
 long long a[MAXN];
--- code/number-theory/MatrixFastPow.cpp
+++ code/number-theory/MatrixFastPow.cpp
@@ -1,24 +1,25 @@
 typedef vector<vector<ll> > mtx;
 typedef vector<ll> row;
-inline int nr(mtx& a) {return (int)a.size();}
-int nc(mtx& a) {return (int)a[0].size();}
-void mul(mtx& a, mtx& b) { // multiply in-place
+inline int nr(mtx& a) { return (int)a.size(); }
+int nc(mtx& a) { return (int)a[0].size(); }
+void mul(mtx& a, mtx& b) {  // multiply in-place
     mtx c = mtx(nr(a), row(nc(b), 0));
     for (int i = 0; i < nr(a); i++)
-		for (int j = 0; j < nc(b); j++)
-			for (int k = 0; k < nc(a); k++) {
-				c[i][j] += a[i][k] * b[k][j];
+        for (int j = 0; j < nc(b); j++)
+            for (int k = 0; k < nc(a); k++) {
+                c[i][j] += a[i][k] * b[k][j];
                 if (c[i][j] >= MOD) c[i][j] %= MOD;
-			}
-a = c; }
+            }
+    a = c;
+}
 mtx pw(mtx& a, ll n) {
     mtx r = a;
-	for (int i = 0; i < nr(a); i++)
-		for (int j = 0; j < nr(a); j++)
-			r[i][j] = (i == j):
-    while (n > 0) {
-        if (n & 1) mul(r, a);
-        mul(a, a);
-        n >>= 1;
-    }
-return r; }
+    for (int i = 0; i < nr(a); i++)
+        for (int j = 0; j < nr(a); j++)
+            r[i][j] = (i == j) : while (n > 0) {
+                if (n & 1) mul(r, a);
+                mul(a, a);
+                n >>= 1;
+            }
+    return r;
+}
