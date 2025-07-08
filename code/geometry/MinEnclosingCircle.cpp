9537d5f355186a92772b1a052ee9655374fec426
root <howard20051109@gmail.com>
2025-07-08 21:13:21 +0800

    update MinEnclosingCircle

--- code/geometry/MinEnclosingCircle.cpp
+++ code/geometry/MinEnclosingCircle.cpp
@@ -1,3 +1,4 @@
+const int INF = 1e9;
 Pt circumcenter(Pt A, Pt B, Pt C) {
     // a1(x-A.x) + b1(y-A.y) = c1
     // a2(x-A.x) + b2(y-A.y) = c2
@@ -12,22 +13,22 @@ Pt circumcenter(Pt A, Pt B, Pt C) {
 }
 Pt center;
 T r2;
-void minEncloseCircle() {
+void minEncloseCircle(vector<Pt> pts) {
     mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
-    shuffle(ALL(E), gen);
-    center = E[0], r2 = 0;
+    shuffle(pts.begin(), pts.end(), gen);
+    center = pts[0], r2 = 0;
 
-    for (int i = 0; i < n; i++) {
-        if (dis2(center, E[i]) <= r2) continue;
-        center = E[i], r2 = 0;
+    for (int i = 0; i < pts.size(); i++) {
+        if (dis2(center, pts[i]) <= r2) continue;
+        center = pts[i], r2 = 0;
         for (int j = 0; j < i; j++) {
-            if (dis2(center, E[j]) <= r2) continue;
-            center = (E[i] + E[j]) / 2.0;
-            r2 = dis2(center, E[i]);
+            if (dis2(center, pts[j]) <= r2) continue;
+            center = (pts[i] + pts[j]) / 2.0;
+            r2 = dis2(center, pts[i]);
             for (int k = 0; k < j; k++) {
-                if (dis2(center, E[k]) <= r2) continue;
-                center = circumcenter(E[i], E[j], E[k]);
-                r2 = dis2(center, E[i]);
+                if (dis2(center, pts[k]) <= r2) continue;
+                center = circumcenter(pts[i], pts[j], pts[k]);
+                r2 = dis2(center, pts[i]);
             }
         }
     }
