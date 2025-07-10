d5f9fd04fc3b4565dda2c7e0fb601983a3af0643
Linus <chengyin30069@gmail.com>
2025-07-10 22:12:34 +0800

    [Update]
    Formated the GeometryDefault.cpp file and compiled the main.pdf file.

--- code/geometry/GeometryDefault.cpp
+++ code/geometry/GeometryDefault.cpp
@@ -8,29 +8,27 @@ short sgn(T x) {
 }
 
 struct Pt {
-T x, y;
-Pt(T _x=0, T _y=0):x(_x), y(_y) {}
-Pt operator+(Pt a) { return Pt(x+a.x, y+a.y); }
-Pt operator-(Pt a) { return Pt(x-a.x, y-a.y); }
-Pt operator*(T a)  { return Pt(x*a, y*a); }
-Pt operator/(T a)  { return Pt(x/a, y/a); }
-T operator*(Pt a)  { return x*a.x + y*a.y; }
-T operator^(Pt a)  { return x*a.y - y*a.x; }
-bool operator<(Pt a)
-    { return x < a.x || (x == a.x && y < a.y); }
-//return sgn(x-a.x) < 0 || (sgn(x-a.x) == 0 && sgn(y-a.y) < 0); }
-bool operator==(Pt a)
-    { return sgn(x-a.x) == 0 && sgn(y-a.y) == 0; }
+    T x, y;
+    Pt(T _x = 0, T _y = 0) : x(_x), y(_y) {}
+    Pt operator+(Pt a) { return Pt(x + a.x, y + a.y); }
+    Pt operator-(Pt a) { return Pt(x - a.x, y - a.y); }
+    Pt operator*(T a) { return Pt(x * a, y * a); }
+    Pt operator/(T a) { return Pt(x / a, y / a); }
+    T operator*(Pt a) { return x * a.x + y * a.y; }
+    T operator^(Pt a) { return x * a.y - y * a.x; }
+    bool operator<(Pt a) { return x < a.x || (x == a.x && y < a.y); }
+    // return sgn(x-a.x) < 0 || (sgn(x-a.x) == 0 && sgn(y-a.y) < 0); }
+    bool operator==(Pt a) { return sgn(x - a.x) == 0 && sgn(y - a.y) == 0; }
 };
 
-Pt mv(Pt a, Pt b) { return b-a; }
-T len2(Pt a) { return a*a; }
-T dis2(Pt a, Pt b) { return len2(b-a); }
+Pt mv(Pt a, Pt b) { return b - a; }
+T len2(Pt a) { return a * a; }
+T dis2(Pt a, Pt b) { return len2(b - a); }
 
-short ori(Pt a, Pt b) { return ((a^b)>0) - ((a^b)<0); }
+short ori(Pt a, Pt b) { return ((a ^ b) > 0) - ((a ^ b) < 0); }
 bool onseg(Pt p, Pt l1, Pt l2) {
     Pt a = mv(p, l1), b = mv(p, l2);
-    return ((a^b) == 0) && ((a*b) <= 0);
+    return ((a ^ b) == 0) && ((a * b) <= 0);
 }
 bool argcmp(Pt u, Pt v) {
     auto half = [](const Pt& p) {
