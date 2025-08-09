e60034c059b9cc9f20b13f7db0b425f806d38973
Howardleejhenhao <howard20051109@gmail.com>
2025-08-09 16:54:55 +0800

    [Update]

--- code/flow/Dinic.cpp
+++ code/flow/Dinic.cpp
@@ -1,61 +1,56 @@
 struct Dinic {
-    int n, s, t, level[N], iter[N];
-    struct edge {
-        int to, cap, rev;
-    };
-    vector<edge> path[N];
-    void init(int _n, int _s, int _t) {
-        n = _n, s = _s, t = _t;
-        FOR(i, 0, n + 1)
-        path[i].clear();
+    struct Edge { int to, cap, rev; };
+    int n, s, t;
+    vector<vector<Edge>> g;
+    vector<int> level, it;
+
+    void init(int _n, int _s, int _t){
+        n=_n; s=_s; t=_t;
+        g.assign(n, {});
+        level.assign(n, 0);
+        it.assign(n, 0);
     }
-    void add(int a, int b, int c) {
-        edge now;
-        now.to = b, now.cap = c, now.rev = sz(path[b]);
-        path[a].pb(now);
-        now.to = a, now.cap = 0, now.rev = sz(path[a]) - 1;
-        path[b].pb(now);
+    void add(int a,int b,int c){
+        Edge f{b,c,(int)g[b].size()};
+        Edge r{a,0,(int)g[a].size()};
+        g[a].push_back(f);
+        g[b].push_back(r);
     }
-    void bfs() {
-        memset(level, -1, sizeof(level));
-        level[s] = 0;
-        queue<int> q;
-        q.push(s);
-        while (q.size()) {
-            int now = q.front();
-            q.pop();
-            for (edge e : path[now]) {
-                if (e.cap > 0 && level[e.to] == -1) {
-                    level[e.to] = level[now] + 1;
+    bool bfs(){
+        fill(level.begin(), level.end(), -1);
+        queue<int> q; level[s]=0; q.push(s);
+        while(!q.empty()){
+            int u=q.front(); q.pop();
+            for(const auto &e: g[u]){
+                if(e.cap>0 && level[e.to]==-1){
+                    level[e.to]=level[u]+1;
                     q.push(e.to);
                 }
             }
         }
+        return level[t]!=-1;
     }
-    int dfs(int now, int flow) {
-        if (now == t) return flow;
-        for (int &i = iter[now]; i < sz(path[now]); i++) {
-            edge &e = path[now][i];
-            if (e.cap > 0 && level[e.to] == level[now] + 1) {
-                int res = dfs(e.to, min(flow, e.cap));
-                if (res > 0) {
-                    e.cap -= res;
-                    path[e.to][e.rev].cap += res;
-                    return res;
+    int dfs(int u,int f){
+        if(!f || u==t) return f;
+        for(int &i=it[u]; i<(int)g[u].size(); ++i){
+            auto &e=g[u][i];
+            if(e.cap>0 && level[e.to]==level[u]+1){
+                int got=dfs(e.to, min(f, e.cap));
+                if(got){
+                    e.cap-=got;
+                    g[e.to][e.rev].cap+=got;
+                    return got;
                 }
             }
         }
         return 0;
     }
-    int dinic() {
-        int res = 0;
-        while (true) {
-            bfs();
-            if (level[t] == -1) break;
-            memset(iter, 0, sizeof(iter));
-            int now = 0;
-            while ((now = dfs(s, INF)) > 0) res += now;
+    int maxflow(){
+        int flow=0, add;
+        while(bfs()){
+            fill(it.begin(), it.end(), 0);
+            while((add=dfs(s, INF))) flow+=add;
         }
-        return res;
+        return flow;
     }
 };
