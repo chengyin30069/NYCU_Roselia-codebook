2031ea23bac172291975cb3082ffa9d9d1e404d3
Linus <chengyin30069@gmail.com>
2025-11-14 02:10:54 +0800

    Fixed li-chao-tree.cpp. NOT COMPILED YET

--- code/data-structure/li-chao-tree.cpp
+++ code/data-structure/li-chao-tree.cpp
@@ -1,25 +1,57 @@
-constexpr int maxn = 5e4 + 5;
-struct line {
-    ld a, b;
-    ld operator()(ld x) { return a * x + b; }
-} arr[(maxn + 1) << 2];
-bool operator<(line a, line b) { return a.a < b.a; }
-#define m ((l + r) >> 1)
-void insert(line x, int i = 1, int l = 0, int r = maxn) {
-    if (r - l == 1) {
-        if (x(l) > arr[i](l))
-            arr[i] = x;
-        return;
-    }
-    line a = max(arr[i], x), b = min(arr[i], x);
-    if (a(m) > b(m))
-        arr[i] = a, insert(b, i << 1, l, m);
-    else
-        arr[i] = b, insert(a, i << 1 | 1, m, r);
+const int eps=1e-9;
+struct line
+{
+     double m,k;
+}p[N];
+int seg[N<<2],cnt;
+double cal(int id,int x){return 1.0*p[id].m*x+p[id].k;}
+void add(int x0,int y0,int x1,int y1)
+{
+     cnt++;
+     if(x0==x1)p[cnt].m=0,p[cnt].k=max(y0,y1);
+     else p[cnt].m=1.0*(y1-y0)/(x1-x0),p[cnt].k=y0-p[cnt].m*x0;
 }
-ld query(int x, int i = 1, int l = 0, int r = maxn) {
-    if (x < l || r <= x) return -numeric_limits<ld>::max();
-    if (r - l == 1) return arr[i](x);
-    return max({arr[i](x), query(x, i << 1, l, m), query(x, i << 1 | 1, m, r)});
+void update(int x,int l,int r,int ql,int qr,int u)
+{
+     int v=seg[x],mid=(l+r)>>1;
+     double resu=cal(u,mid),resv=cal(v,mid);
+     if(qr<l||r<ql)return;
+     if(ql<=l&&r<=qr)
+     {
+          if(l==r)
+          {
+               if(resu>resv)seg[x]=u;
+               return;
+          }
+          if(p[u].m-p[v].m>eps)
+          {
+               if(resu-resv>eps)
+               {
+                    seg[x]=u;
+                    update(x<<1,l,mid,ql,qr,v);
+               }
+               else update(x<<1|1,mid+1,r,ql,qr,u);
+          }
+          else if(p[v].m-p[u].m>eps)
+          {
+               if(resu-resv>eps)
+               {
+                    seg[x]=u;
+                    update(x<<1|1,mid+1,r,ql,qr,v);
+               }
+               else update(x<<1,l,mid,ql,qr,u);
+          }
+          else if(p[u].k-p[v].k>eps)seg[x]=u;
+          return;
+     }
+     update(x<<1,l,mid,ql,qr,u);
+     update(x<<1|1,mid+1,r,ql,qr,u);
 }
-#undef m
+double ask(int x,int l,int r,int qx)
+{
+     if(r<qx||qx<l)return{0,0};
+     int mid=(l+r)>>1;
+     double res=cal(seg[x],qx);
+     if(l==r)return res;
+     return max({res,seg[x],ask(x<<1,l,mid,qx),ask(x<<1|1,mid+1,r,qx)});
+}
