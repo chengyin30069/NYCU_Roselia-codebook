113180f0978e85819cc44db24091882e20295a76
Linus <chengyin30069@gmail.com>
2025-11-13 15:46:44 +0800

    Update Sparse Table

--- code/data-structure/SparseTable.cpp
+++ code/data-structure/SparseTable.cpp
@@ -1,18 +1,14 @@
-const int lgmx = 19;
-
-int n, q;
-int spt[lgmx][maxn];
-
-void build() {
-    FOR(k, 1, lgmx, 1) {
-        for (int i = 0; i + (1 << k) - 1 < n; i++) {
-            spt[k][i] = min(spt[k - 1][i], spt[k - 1][i + (1 << (k - 1))]);
-        }
-    }
+int a[N];
+int st[N][30];
+void pre(int n)
+{
+     FOR(i,1,n+1)st[i][0]=a[i];
+     for(int j=1;(1<<j)<=n+1;j++)
+          for(int i=0;i+(1<<j)<=n+1;i++)
+               st[i][j]=min(st[i][j-1],st[i+(1<<(j-1))][j-1]);
 }
-
-int query(int l, int r) {
-    int ln = len(l, r);
-    int lg = __lg(ln);
-    return min(spt[lg][l], spt[lg][r - (1 << lg) + 1]);
+int ask(int l,int r)
+{
+     int k=__lg(r-l+1);
+     return min(st[l][k],st[r-(1<<k)+1][k]);
 }
