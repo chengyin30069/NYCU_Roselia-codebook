dfede2c1409a6a50cd7c68fa7e0a4bc6351c38b4
Linus <chengyin30069@gmail.com>
2025-12-29 15:15:01 +0800

    Add get_min_cov() for HopcroftKarp.cpp Better format for KM.cpp Recompiled

--- code/flow/HopcroftKarp.cpp
+++ code/flow/HopcroftKarp.cpp
@@ -1,13 +1,15 @@
 struct HopcroftKarp {
-    // id: X = [1, nx], Y = [nx+1, nx+ny]
-    int n, nx, ny, m, MXCNT;
+    int n, nx, ny;  // X: [1, nx], Y: [nx+1, nx+ny]
     vector<vector<int> > g;
-    vector<int> mx, my, dis, vis;
-    void init(int nnx, int nny, int mm) {
-        nx = nnx, ny = nny, m = mm;
+    vector<int> mx, my, dis, vis, min_cov;
+    void init(int nnx, int nny) {
+        nx = nnx, ny = nny;
         n = nx + ny + 1;
-        g.clear();
-        g.resize(n);
+        g.resize(n, vector<int>(0));
+        mx.resize(n, -1);
+        my.resize(n, -1);
+        dis.resize(n, -1);
+        vis.resize(n, 0);
     }
     void add(int x, int y) {
         g[x].emplace_back(y);
@@ -15,7 +17,7 @@ struct HopcroftKarp {
     }
     bool dfs(int x) {
         vis[x] = true;
-        Each(y, g[x]) {
+        for (auto y : g[x]) {
             int px = my[y];
             if (px == -1 ||
                 (dis[px] == dis[x] + 1 &&
@@ -27,16 +29,11 @@ struct HopcroftKarp {
         }
         return false;
     }
-    void get() {
-        mx.clear();
-        mx.resize(n, -1);
-        my.clear();
-        my.resize(n, -1);
-
+    int get() {
+        int res = 0;
         while (true) {
             queue<int> q;
-            dis.clear();
-            dis.resize(n, -1);
+            fill(dis.begin(), dis.end(), -1);
             for (int x = 1; x <= nx; x++) {
                 if (mx[x] == -1) {
                     dis[x] = 0;
@@ -46,25 +43,49 @@ struct HopcroftKarp {
             while (!q.empty()) {
                 int x = q.front();
                 q.pop();
-                Each(y, g[x]) {
-                    if (my[y] != -1 && dis[my[y]] == -1) {
+                for (auto y : g[x]) {
+                    if (my[y] != -1 &&
+                        dis[my[y]] == -1) {
                         dis[my[y]] = dis[x] + 1;
                         q.push(my[y]);
                     }
                 }
             }
-
             bool brk = true;
-            vis.clear();
-            vis.resize(n, 0);
+            fill(vis.begin(), vis.end(), 0);
             for (int x = 1; x <= nx; x++)
                 if (mx[x] == -1 && dfs(x))
                     brk = false;
-
             if (brk) break;
         }
-        MXCNT = 0;
         for (int x = 1; x <= nx; x++)
-            if (mx[x] != -1) MXCNT++;
+            if (mx[x] != -1) res++;
+        return res;
+    }
+    void get_min_cov() {
+        vector<int> visX(n, 0), visY(n, 0);
+        queue<int> q;
+        min_cov.resize(n, 0);
+        for (int x = 1; x < nx + 1; x++)
+            if (mx[x] == -1) {
+                visX[x] = 1;
+                q.push(x);
+            }
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+            for (auto y : g[x])
+                if (!visY[y]) {
+                    visY[y] = 1;
+                    if (my[y] != -1 && !visX[my[y]]) {
+                        visX[my[y]] = 1;
+                        q.push(my[y]);
+                    }
+                }
+        }
+        for (int x = 1; x < nx + 1; x++)
+            if (!visX[x]) min_cov[x] = 1;
+        for (int y = nx + 1; y < n; y++)
+            if (visY[y]) min_cov[y] = 1;
     }
 } hk;
--- code/flow/KM.cpp
+++ code/flow/KM.cpp
@@ -1,11 +1,11 @@
-struct KM { // 1-based
+struct KM {  // 1-based
     int n, mx[1005], my[1005], pa[1005];
     int g[1005][1005], lx[1005], ly[1005], sy[1005];
     bool vx[1005], vy[1005];
     void init(int _n) {
         n = _n;
-        FOR(i, 1, n + 1)
-        	fill(g[i], g[i] + 1 + n, 0);
+        for (int i = 1; i < n + 1; i++)
+            fill(g[i], g[i] + 1 + n, 0);
     }
     void add(int a, int b, int c) { g[a][b] = c; }
     void augment(int y) {
@@ -13,14 +13,16 @@ struct KM { // 1-based
             x = pa[y], z = mx[x], my[y] = x, mx[x] = y;
     }
     void bfs(int st) {
-        FOR(i, 1, n + 1) sy[i] = INF, vx[i] = vy[i] = 0;
-        queue<int> q; q.push(st);
+        for (int i = 1; i < n + 1; i++)
+            sy[i] = INF, vx[i] = vy[i] = 0;
+        queue<int> q;
+        q.push(st);
         for (;;) {
             while (!q.empty()) {
                 int x = q.front();
                 q.pop();
                 vx[x] = 1;
-                FOR(y, 1, n + 1)
+                for (int y = 1; y < n + 1; y++) 
                 if (!vy[y]) {
                     int t = lx[x] + ly[y] - g[x][y];
                     if (t == 0) {
@@ -30,19 +32,19 @@ struct KM { // 1-based
                             return;
                         }
                         vy[y] = 1, q.push(my[y]);
-                    } 
-					else if (sy[y] > t) pa[y] = x, sy[y] = t;
+                    } else if (sy[y] > t)
+                        pa[y] = x, sy[y] = t;
                 }
             }
             int cut = INF;
-            FOR(y, 1, n + 1)
-            if (!vy[y] && cut > sy[y]) cut = sy[y];
-            FOR(j, 1, n + 1) {
+            for (int y = 1; y < n + 1; y++)
+                if (!vy[y] && cut > sy[y]) cut = sy[y];
+            for (int j = 1; j < n + 1; j++) {
                 if (vx[j]) lx[j] -= cut;
                 if (vy[j]) ly[j] += cut;
                 else sy[j] -= cut;
             }
-            FOR(y, 1, n + 1) {
+            for (int y = 1; y < n + 1; y++) {
                 if (!vy[y] && sy[y] == 0) {
                     if (!my[y]) {
                         augment(y);
@@ -59,12 +61,13 @@ struct KM { // 1-based
         fill(my, my + n + 1, 0);
         fill(ly, ly + n + 1, 0);
         fill(lx, lx + n + 1, 0);
-        FOR(x, 1, n + 1)FOR(y, 1, n + 1)
-        	lx[x] = max(lx[x], g[x][y]);
-        FOR(x, 1, n + 1)bfs(x);
+        for (int x = 1; x < n + 1; x++)
+            for (int y = 1; y < n + 1; y++)
+                lx[x] = max(lx[x], g[x][y]);
+        for (int x = 1; x < n + 1; x++) bfs(x);
         int ans = 0;
-        FOR(y, 1, n + 1)
-        	ans += g[my[y]][y];
+        for (int y = 1; y < n + 1; y++)
+            ans += g[my[y]][y];
         return ans;
     }
 };
